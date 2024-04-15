79789db92a14d86278e17ffe0b7110abfabd9fa7
Kevin Müller <kevin.mueller@tuta.com>
2024-04-15 18:54:25 +0200

    finished design of Footer, TODO: set up right links

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  links:string[] =['impressum','impressum','impressum']
+
+  linkToPage(index:number){
+    window.location.href =`${this.links[index]}`;
+  }
 
 }
