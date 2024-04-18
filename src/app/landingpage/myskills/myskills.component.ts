fe8e556f2704c31968d4246ffd7c47ad4a1134d4
Kevin Mueller <kevin.mueller@tuta.com>
2024-04-18 14:14:08 +0200

    set up template ngfor

--- src/app/landingpage/myskills/myskills.component.ts
+++ src/app/landingpage/myskills/myskills.component.ts
@@ -9,5 +9,47 @@ import { CommonModule } from '@angular/common';
   styleUrl: './myskills.component.scss'
 })
 export class MyskillsComponent {
+  items = [
+    {
+      url: '/assets/img/icon/angular.svg',
+      title: 'Angular'
+    },
+    {
+      url: '/assets/img/icon/typescript.svg',
+      title: 'TypeScript'
+    },
+    {
+      url: '/assets/img/icon/javascript.svg',
+      title: 'JavaScript'
+    },
+    {
+      url: '/assets/img/icon/html.svg',
+      title: 'HTML'
+    },
+    {
+      url: '/assets/img/icon/firebase.svg',
+      title: 'Firebase'
+    },
+    {
+      url: '/assets/img/icon/git.svg',
+      title: 'GIT'
+    },
+    {
+      url: '/assets/img/icon/css.svg',
+      title: 'CSS'
+    },
+    {
+      url: '/assets/img/icon/restapi.svg',
+      title: 'Rest-Api'
+    },
+    {
+      url: '/assets/img/icon/scrum.svg',
+      title: 'Scrum'
+    },
+    {
+      url: '/assets/img/icon/material.svg',
+      title: 'Material design'
+    }
+  ];
 
 }
