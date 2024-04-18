243da468913df3f2b411852316e162326c3f4305
Kevin Mueller <kevin.mueller@tuta.com>
2024-04-18 08:54:23 +0200

    added aboutme component and finished description

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AtfSectionComponent } from './atf-section/atf-section.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [CommonModule, AtfSectionComponent],
+  imports: [CommonModule, AtfSectionComponent, AboutmeComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
