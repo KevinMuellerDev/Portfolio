548b07d88d16ca1ef7c9a859feebf0061ba8f788
Kevin Mueller <kevin.mueller@tuta.com>
2024-04-22 10:36:45 +0200

    added header of portfolio

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { AtfSectionComponent } from './atf-section/atf-section.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { MyskillsComponent } from './myskills/myskills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [CommonModule, AtfSectionComponent, AboutmeComponent, MyskillsComponent],
+  imports: [CommonModule, AtfSectionComponent, AboutmeComponent, MyskillsComponent,PortfolioComponent],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
