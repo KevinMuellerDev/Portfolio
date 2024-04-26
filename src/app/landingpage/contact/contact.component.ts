f610e4d1d19726231f1c25d450a8c621edd2edc4
Kevin Mueller <kevin.mueller@tuta.com>
2024-04-26 11:12:49 +0200

    added contactform design

--- src/app/landingpage/contact/contact.component.ts
+++ src/app/landingpage/contact/contact.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
 }
