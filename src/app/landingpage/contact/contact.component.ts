ec1d1b614c77f44fe5aeb66f1b205c0543643240
Kevin Müller <kevin.mueller@tuta.com>
2024-04-27 13:14:23 +0200

    added form logic

--- src/app/landingpage/contact/contact.component.ts
+++ src/app/landingpage/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
@@ -11,6 +12,8 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class ContactComponent {
 
+  http = inject(HttpClient);
+
 
   contactData = {
     name: '',
@@ -18,4 +21,35 @@ export class ContactComponent {
     message: '',
   };
 
+  mailTest = false;
+
+  post = {
+    endPoint: 'https://kevin-mueller-dev.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
 }
