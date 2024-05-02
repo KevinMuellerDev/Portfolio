9c0f4ee7bb1f4ed128bd85cae352b7b3d0f7da62
Kevin Müller <kevin.mueller@tuta.com>
2024-05-02 11:56:42 +0200

    added responsive for my skills

--- src/app/landingpage/myskills/myskills.component.ts
+++ src/app/landingpage/myskills/myskills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,45 +12,67 @@ export class MyskillsComponent {
   skills = [
     {
       url: '/assets/img/icon/angular.svg',
+      urlMobile: '/assets/img/icon/angularmobile.svg',
       title: 'Angular'
     },
     {
       url: '/assets/img/icon/typescript.svg',
+      urlMobile: '/assets/img/icon/typescriptmobile.svg',
       title: 'TypeScript'
     },
     {
       url: '/assets/img/icon/javascript.svg',
+      urlMobile: '/assets/img/icon/javascriptmobile.svg',
       title: 'JavaScript'
     },
     {
       url: '/assets/img/icon/html.svg',
+      urlMobile: '/assets/img/icon/htmlmobile.svg',
       title: 'HTML'
     },
     {
       url: '/assets/img/icon/firebase.svg',
+      urlMobile: '/assets/img/icon/firebasemobile.svg',
       title: 'Firebase'
     },
     {
       url: '/assets/img/icon/git.svg',
+      urlMobile: '/assets/img/icon/gitmobile.svg',
       title: 'GIT'
     },
     {
       url: '/assets/img/icon/css.svg',
+      urlMobile: '/assets/img/icon/cssmobile.svg',
       title: 'CSS'
     },
     {
       url: '/assets/img/icon/restapi.svg',
+      urlMobile: '/assets/img/icon/restapimobile.svg',
       title: 'Rest-Api'
     },
     {
       url: '/assets/img/icon/scrum.svg',
+      urlMobile: '/assets/img/icon/scrummobile.svg',
       title: 'Scrum'
     },
     {
       url: '/assets/img/icon/material.svg',
+      urlMobile: '/assets/img/icon/materialmobile.svg',
       title: 'Material design'
     }
   ];
+
+  public screenWidth: any;
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    this.screenWidth = window.innerWidth;
+
+  }
 
   navigateToContact(){
     window.location.href="#contact"
