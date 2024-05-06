5362bb9c2af0b9f6c686c839e93723e1f5ba8412
Kevin Mueller <kevin.mueller@tuta.com>
2024-05-06 08:34:14 +0200

    added burger slidemenu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,18 +6,31 @@ import { AppComponent } from '../../app.component';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,AppComponent],
+  imports: [CommonModule, AppComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  isTarget:string = '';
-  about:string = 'about';
-  skills:string = 'skills';
-  portfolio:string = 'portfolio';
+  isTarget: string = '';
+  about: string = 'about';
+  skills: string = 'skills';
+  portfolio: string = 'portfolio';
 
-  
-  navigateHome(){
-    window.scrollTo(0 , document.body.scrollTop);
+  menuOpen: boolean = false;
+
+  navigateHome() {
+    window.scrollTo(0, document.body.scrollTop);
   }
+
+  noScroll() {
+    if (!this.menuOpen) {
+      document.getElementsByTagName('app-root')[0].classList.add('modal-open');
+      this.menuOpen = true;
+    } else {
+      document.getElementsByTagName('app-root')[0].classList.remove('modal-open');
+      this.menuOpen = false;
+    }
+  }
+
+
 }
