43d2b28caa61cc481a35594789b71525d696885b
Kevin Mueller <kevin.mueller@tuta.com>
2024-07-11 08:59:38 +0200

    added da bubble as project

--- src/app/landingpage/portfolio/projectdata.service.ts
+++ src/app/landingpage/portfolio/projectdata.service.ts
@@ -6,12 +6,13 @@ import { Injectable } from '@angular/core';
 export class ProjectdataService {
   projects = [
     {
-      title: "Join",
-      stack: "JavaScript | HTML | CSS",
-      description: "join_desc",
-      image: "/assets/img/joinphoto.png",
-      liveUrl: "http://join.kevin-mueller-dev.de/",
-      githubUrl: "https://github.com/KevinMuellerDev/join-Kanban"
+      title: "DA Bubble",
+      stack: "Angular | Typescript | SASS | Firebase ",
+      description: "dabubble_desc",
+      image: "/assets/img/halloweenphoto.png",
+      liveUrl: "http://dabubble.kevin-mueller-dev.de/",
+      githubUrl: "https://github.com/KevinMuellerDev/da-bubble"
+
     },
     {
       title: "Halloween Madness",
@@ -21,7 +22,16 @@ export class ProjectdataService {
       liveUrl: "http://halloween-madness.kevin-mueller-dev.de/",
       githubUrl: "https://github.com/KevinMuellerDev/halloween-madness"
 
+    },
+    {
+      title: "Join",
+      stack: "JavaScript | HTML | CSS",
+      description: "join_desc",
+      image: "/assets/img/joinphoto.png",
+      liveUrl: "http://join.kevin-mueller-dev.de/",
+      githubUrl: "https://github.com/KevinMuellerDev/join-Kanban"
     }
+
   ]
   constructor() { }
 }
